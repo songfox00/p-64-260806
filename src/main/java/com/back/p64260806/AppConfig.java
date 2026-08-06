fb4b6a00b155256c94289e1b7abbb7341619d7a0
songfox00 <songfox00@gmail.com>
2026-08-06 11:43:14 +0900

    ApplicationRunner, order

--- src/main/java/com/back/p64260806/AppConfig.java
+++ src/main/java/com/back/p64260806/AppConfig.java
@@ -1,7 +1,9 @@
 package com.back.p64260806;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
@@ -16,4 +18,33 @@ public class AppConfig {
         return new PersonRepository(2);
     }
 
+    @Bean
+    public ApplicationRunner myApplicationRunner3() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    @Bean
+    @Order(2)
+    public ApplicationRunner applicationRunner2(){
+        System.out.println("myApplicationRunner2");
+        return new MyApplicationRunner(2);
+    }
+
+    @Bean
+    @Order(1)
+    public ApplicationRunner applicationRunner1(){
+        System.out.println("myApplicationRunner1");
+        return new MyApplicationRunner(1);
+    }
+
+    public void work1() {
+        System.out.println("work1");
+    }
+
+    public void work2() {
+        System.out.println("work2");
+    }
 }
---
+++ src/main/java/com/back/p64260806/MyApplicationRunner.java
@@ -0,0 +1,15 @@
+package com.back.p64260806;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+@RequiredArgsConstructor
+public class MyApplicationRunner implements ApplicationRunner {
+    private final int version;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyApplicationRunner.run" + ", version = " + version);
+    }
+}
